7ad7a673a8c95f679c2e0f90ae8dfa5f0f80dcf9
yaocccc <chen.yaocan@infoloop.cn>
2020-09-23 20:33:08 +0800

    remove some need't config

--- config.h
+++ config.h
@@ -193,10 +193,6 @@ static MouseShortcut mshortcuts[] = {
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 1}         },
 	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 1}         },
-	/* { ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
-	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
-	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
-	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} }, */
 };
 
 /* Internal keyboard shortcuts. */
