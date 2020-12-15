c5f7484894ddc4cab02c0a32b311530bd7b05d22
chenyc <chen.yaocan@infoloop.cn>
2020-12-15 09:58:11 +0800

    update chscale

--- config.h
+++ config.h
@@ -27,7 +27,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
-static float chscale = 1.0;
+static float chscale = 0.85;
 
 /*
  * word delimiter string
