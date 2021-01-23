c6d45c9f0db9da4f684a03e5fbeecc54b6a3046b
chenyc <flyforest@outlook.com>
2021-01-23 20:31:46 +0800

    打连体字补丁

--- st.c
+++ st.c
@@ -2658,7 +2658,8 @@ draw(void)
 	drawregion(0, 0, term.col, term.row);
  	if (term.scr == 0)
  		xdrawcursor(cx, term.c.y, term.line[term.c.y][cx],
- 				term.ocx, term.ocy, term.line[term.ocy][term.ocx]);
+ 				term.ocx, term.ocy, term.line[term.ocy][term.ocx],
+                term.line[term.ocy], term.col);
 	term.ocx = cx;
 	term.ocy = term.c.y;
 	xfinishdraw();
@@ -2718,7 +2719,8 @@ void select_or_drawcursor(int selectsearch_mode, int type) {
 	}
 	else
 		xdrawcursor(term.c.x, term.c.y, term.line[term.c.y][term.c.x],
-			    term.ocx, term.ocy, term.line[term.ocy][term.ocx]);
+			    term.ocx, term.ocy, term.line[term.ocy][term.ocx],
+                term.line[term.ocy], term.col);
 }
 
 void search(int selectsearch_mode, Rune *target, int ptarget, int incr, int type, TCursor *cu) {
