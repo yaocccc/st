909adcf4119eda25f7523a62e477adf3104b1e9c
chenyc <flyforest@outlook.com>
2022-11-24 12:14:27 +0800

    update size

--- config.h
+++ config.h
@@ -157,8 +157,8 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 80;
-static unsigned int rows = 24;
+static unsigned int cols = 100;
+static unsigned int rows = 30;
 
 /*
  * Default colour and shape of the mouse cursor
